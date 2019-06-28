21232463ba2852ee1062d038c886e7508fe2412a
Martin Komischke <martin.komischke@outlook.com>
2019-06-28 20:11:34 +0200

    Updates

---
+++ precache-manifest.df0e3a1ecaabce482050682e9a1d30b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddad2a91174c6375df4dd8924a4043b4",
+    "url": "/agile-principles-pinboard/index.html"
+  },
+  {
+    "revision": "cefe539ad20e87981971",
+    "url": "/agile-principles-pinboard/static/css/main.bae096c6.chunk.css"
+  },
+  {
+    "revision": "4f2509e10a722f0850f0",
+    "url": "/agile-principles-pinboard/static/js/2.ed8f3dea.chunk.js"
+  },
+  {
+    "revision": "cefe539ad20e87981971",
+    "url": "/agile-principles-pinboard/static/js/main.384c3f7f.chunk.js"
+  },
+  {
+    "revision": "89615b079486c0acc38d",
+    "url": "/agile-principles-pinboard/static/js/runtime~main.bae0fbff.js"
+  }
+]);
