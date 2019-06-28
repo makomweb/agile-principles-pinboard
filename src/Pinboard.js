6242c4a98577dd95f3baebc33e6fd55a562d715a
Martin Komischke <martin.komischke@outlook.com>
2019-06-28 15:16:57 +0200

    hover to lightgreen when dragging over the pinboard

--- src/Pinboard.js
+++ src/Pinboard.js
@@ -12,19 +12,20 @@ function collect(connect, monitor) {
 class Pinboard extends Component {
     render() {
         const { connectDropTarget, hovered, pinned } = this.props;
+        const backgroundColor = hovered ? 'lightgreen' : 'brown';
 
         if (pinned) {
             return connectDropTarget(
-                <div className="grid pinboard">
+                <div className="grid pinboard" style={{background: backgroundColor}}>
                     <div className="centered">
                         {pinned.text}
                     </div>
                 </div>
             );
         }
-        
+
         return connectDropTarget(
-            <div className="grid pinboard">
+            <div className="grid pinboard" style={{background: backgroundColor}}>
                 <div className="centered">
                     Drag a card here.
                 </div>
